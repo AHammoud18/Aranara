94e8a77f4e0192d15c576624b75eb464a7c7a7b8
ahammoud <alihammoud120@gmail.com>
2022-10-01 14:13:52 -0400

    initial commit...ARANARA

--- Aranara/ContentView.swift
+++ Aranara/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var moveY = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Ararycan
+                .offset(y: moveY ? -20 : 0)
+                .onTapGesture(){
+                    withAnimation(.easeInOut(duration: 2).repeatForever()){
+                        moveY.toggle()
+                    }
+                }
+
+        }
     }
+}
+
+var Ararycan : some View{
+    Image("Ararycan")
+        .resizable()
+        .padding()
+        .frame(width: 325, height: 325)
+        .offset(x:-250, y:0)
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .ignoresSafeArea(.all)
+            .previewInterfaceOrientation(   .landscapeLeft)
     }
 }
