ec148e226cf98b1fa3c00d959cca71a29423b2df
ahammoud <alihammoud120@gmail.com>
2022-10-01 13:11:25 -0400

    Initial Commit

---
+++ Aranara/AranaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AranaraApp.swift
+//  Aranara
+//
+//  Created by Ali Hamoud on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AranaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
