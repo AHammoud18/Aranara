0abf8e95fd598067f30e2c6a6f645a4f857ce52b
ahammoud <alihammoud120@gmail.com>
2022-10-02 10:05:17 -0400

    branch created to make changes

--- Aranara/ContentView.swift
+++ Aranara/ContentView.swift
@@ -9,27 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     @State var moveY = false
+    @State var opacity = 0.0
+    //@State var getSizeX: Int?
+    //@State var getSizeY: Int?
     var body: some View {
         ZStack{
+            Background
+                .blur(radius: 3)
             Ararycan
-                .offset(y: moveY ? -20 : 0)
-                .onTapGesture(){
-                    withAnimation(.easeInOut(duration: 2).repeatForever()){
-                        moveY.toggle()
-                    }
-                }
+            Greeting
 
+                
         }
+    }
+    
+    var Ararycan : some View{
+        Image("Ararycan")
+            .resizable()
+            .padding()
+            .frame(width: 325, height: 325)
+            .offset(x:-250, y: moveY ? -20 : 0)
+            .onAppear(){
+                withAnimation(.easeInOut(duration: 2).repeatForever()){
+                    moveY.toggle()
+                }
+            }
+    }
+    
+    var Greeting : some View{
+        Rectangle()
+            .opacity(0)
+            .frame(width: 150 , height: 150)
+            .offset(x:-250 ,y: moveY ? -20 : 0)
+            .onTapGesture(){
+            print("Hi")
+    
+            }
     }
 }
 
-var Ararycan : some View{
-    Image("Ararycan")
+
+var Background : some View{
+    Image("sumeruBackground")
         .resizable()
-        .padding()
-        .frame(width: 325, height: 325)
-        .offset(x:-250, y:0)
+        .frame(width: 1800/2, height: 900/2)
+        
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
