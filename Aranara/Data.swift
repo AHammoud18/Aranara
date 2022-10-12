25a569e3efc31874a05832ea3ca759d75f9a9cc8
ahammoud <alihammoud120@gmail.com>
2022-10-12 08:10:52 -0400

    xml parser implemented

--- Aranara/Data.swift
+++ Aranara/Data.swift
@@ -6,31 +6,29 @@
 //
 
 import Foundation
-import SWXMLHash
+import SWXMLHash  // module to parse xml
+import Alamofire  //to grab xml data from url
 
-let url = NSURL(string: "https://raw.githubusercontent.com/AHammoud18/Aranara/test/Aranara/WeatherData.xml") //grab link of xml
+//let url = NSURL(string: "https://raw.githubusercontent.com/AHammoud18/Aranara/test/Aranara/WeatherData.xml") //grab link of xml
 
 class Data: Identifiable{
+    var xml: XMLIndexer?
+    static let shared = Data()
+    var xmlToParse =    // hardcoded xml data for now until i figure out how to grab info from external source
+    """
+    xml data here
+
+    """
     
-    let task = URLSession.shared.dataTask(with: url! as URL) {(data, response, error) in
-        // parse xml file
-        if data != nil{
-            let feed = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)! as String
-            let xml = XMLHash.lazy(feed)
-            
-            /*var locationName = xml["Michigan_Weather"]["location"].element!.text
-            var cloudCoverage = xml["Michigan_Weather"]["cloud_coverage"].element!.text
-            var rain = xml["Michigan_Weather"]["rain_chance"].element!.text*/
-            
-            /*var locationName: String
-            var cloudCoverage: String
-            var rain: String*/
-            
-            /*item.location = xml["Michigan_Weather"]["location"].element!.text
-            item.cloudCover = xml["Michigan_Weather"]["cloud_coverage"].element!.text
-            item.rainChance = xml["Michigan_Weather"]["cloud_coverage"].element!.text*/
-        }
-        
+    init(){
+        self.xml = XMLHash.parse(xmlToParse)  // initialze parser
     }
+}
+        
+
+class Fetch: Identifiable{
+    var location = Data.shared.xml![""][""].element?.text  // grab location from xml
+    var cloudCover = Data.shared.xml![""][""].element?.text  // grab cloud cover from xml
+    var rainChance = Data.shared.xml![""][""].element?.text  // grab rain chance from xml
 }
 
